ef4e780d3d373bc3af6438504f558afbe88f6c59
BrianPizz <bpizzimentijr@outlook.com>
2024-11-20 09:42:05 -0500

    create/import components

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,42 @@
 import { useState } from "react";
 import words from "./wordList.json";
+import { HangmanDrawing } from "./HangmanDrawing";
+import { HangmanWord } from "./HangmanWord";
+import { Keyboard } from "./Keyboard";
 
 function App() {
   const [wordToGuess, setWordToGuess] = useState(() => {
     return words[Math.floor(Math.random() * words.length)];
   });
 
-  console.log(wordToGuess)
+  const [guessedLetters, setGuessedLetetrs] = useState<string[]>([]);
 
-  return <h1>Hi</h1>;
+  console.log(wordToGuess);
+
+  return (
+    <div
+      style={{
+        maxWidth: "800px",
+        display: "flex",
+        flexDirection: "column",
+        gap: "2rem",
+        margin: "0 auto",
+        alignItems: "center",
+      }}
+    >
+      <div
+        style={{
+          fontSize: "2rem",
+          textAlign: "center",
+        }}
+      >
+        Lose Win
+      </div>
+      <HangmanDrawing/>
+      <HangmanWord/>
+      <Keyboard/>
+    </div>
+  );
 }
 
 export default App;
